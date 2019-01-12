b01974b366c3653b52d66a9bbb784b1ab4121e10
mutsune <dev@mutsune.com>
2019-01-12 15:56:09 +0900

    test

--- Casks/win-shortcutter.rb
+++ Casks/win-shortcutter.rb
@@ -1,5 +1,5 @@
 cask 'win-shortcutter' do
-  version '3.6'
+  version '3.6.1'
   sha256 '5e1157d4d2b52a11fa4e4ceb832b08a34ec874eb785dfc6d6096ac4256a2ef3f'
 
   url 'http://www.lobotomo.com/products/downloads/WinShortcutter%203.6.dmg'
